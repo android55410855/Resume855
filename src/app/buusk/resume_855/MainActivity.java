71ee4218e104db5f41d128f6d729c729a55f6d1e
android55410855 <sirilakwailom@gmail.com>
2014-09-02 18:20:01 +0700

    Resume

---
+++ src/app/buusk/resume_855/MainActivity.java
@@ -0,0 +1,19 @@
+package app.buusk.resume_855;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        
+    }
+}
